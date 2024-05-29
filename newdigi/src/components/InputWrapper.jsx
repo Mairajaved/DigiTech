c419885544e8c8cbba4eba97e0a1f6a9fb1f485f
Maira Javed <89129372+Mairajaved@users.noreply.github.com>
2024-05-29 16:52:33 +0500

    Add files via upload

---
+++ newdigi/src/components/InputWrapper.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/Wrapper.css'
+
+function InputWrapper() {
+  return (
+    <>
+      <div class="main">
+        <input type="text" placeholder="Enter your email" />
+        <button>
+           Get Started
+        </button>
+    </div>
+    </>
+  )
+}
+
+export default InputWrapper
